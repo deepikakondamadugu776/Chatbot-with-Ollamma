895e6a1d02662c630f0d164f80264573f6b237fd
deepikakondamadugu776 <deepukondamadugu113@gmail.com>
2024-09-15 12:24:59 +0530

    Added venv

--- app.py
+++ app.py
@@ -1,1 +1,53 @@
-print("hello")
+#Chatbot With Ollamma
+import streamlit as st
+
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Chatbot With Ollamma"
+
+#Prompt Template
+prompt=ChatPromptTemplate([
+  ("system","You are a helpful massistant . Please  repsonse to the user queries"),
+  ("user","question:{question}")
+])
+def generate_response(question,llm,temperature,max_tokens):
+  llm=Ollama(model=llm)
+  Output_parser=StrOutputParser()
+  chain=prompt|llm|Output_parser
+  answer=chain.invoke({'question':question})
+  return answer
+
+
+## #Title of the app
+st.title("Q&A Chatbot With Ollama")
+
+
+## Select the OpenAI model
+llm=st.sidebar.selectbox("Select Open Source model",["gemma2:2b"])
+
+## Adjust response parameter
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+
+
+## MAin interface for user input
+st.write("Goe ahead and ask any question")
+user_input=st.text_input("You:")
+
+
+
+if user_input :
+    response=generate_response(user_input,llm,temperature,max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the user input")
